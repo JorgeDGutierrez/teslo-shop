b46d1e3c048162548f3bb488a1f5d0a277cb3e81
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2022-10-02 01:59:03 -0500

    UI Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SWRConfig } from 'swr';
 
 import { lightTheme } from '../themes';
+import { UIProvider } from '../context';
+import { ReactElement } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -12,10 +14,13 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
       }}
     >
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UIProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+
+      </UIProvider>
     </SWRConfig>
   )
 }
