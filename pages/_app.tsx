acb629f87c58b774e343ceadabb1505754a54960
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2022-10-04 23:43:24 -0500

    se soluciono el problema de Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,24 @@ import { SWRConfig } from 'swr';
 
 import { lightTheme } from '../themes';
 import { UIProvider } from '../context';
-import { ReactElement } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return (
-    <SWRConfig
-      value={{
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-      }}
-    >
-      <UIProvider>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <SWRConfig
+            value={{
+                fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+            }}
+        >
+            <UIProvider isMenuOpen={false}>
+                <ThemeProvider theme={lightTheme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
 
-      </UIProvider>
-    </SWRConfig>
-  )
+            </UIProvider>
+
+        </SWRConfig>
+    )
 }
 
 export default MyApp
