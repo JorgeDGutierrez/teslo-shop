69640e1d045fb8e6b8263df8f21597cabfe8a704
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2022-10-23 18:26:15 -0500

    Mostrar productos del carrito de compra

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,15 +1,16 @@
+import { FC, useContext } from "react"
 import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from "@mui/material"
 import { initialData } from "../../database/products"
 import NextLink from 'next/link'
 import { ItemCounter } from "../ui"
-import { FC } from "react"
+import { CartContext } from "../../context"
 
 
-const productsInCart = [
-    initialData.products[0],
-    initialData.products[1],
-    initialData.products[2],
-]
+// const productsInCart = [
+//     initialData.products[0],
+//     initialData.products[1],
+//     initialData.products[2],
+// ]
 
 interface Props {
     editable?: boolean;
@@ -17,12 +18,12 @@ interface Props {
 
 export const CartList: FC<Props> = ({ editable = false }) => {
 
-
+    const { cart } = useContext(CartContext)
 
     return (
         <>
             {
-                productsInCart.map(product => (
+                cart.map(product => (
                     <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
                         <Grid item xs={3} >
                             {/* TODO: llevar a la pagina del producto */}
@@ -30,7 +31,7 @@ export const CartList: FC<Props> = ({ editable = false }) => {
                                 <Link>
                                     <CardActionArea>
                                         <CardMedia
-                                            image={`/products/${product.images[0]}`}
+                                            image={`/products/${product.image}`}
                                             component='img'
                                             sx={{ borderRadius: '5px' }}
                                         />
@@ -45,8 +46,17 @@ export const CartList: FC<Props> = ({ editable = false }) => {
 
                                 {/* Condicional */}
                                 {
-                                    editable ? <ItemCounter />
-                                        : <Typography variant='h5'>3 items</Typography>
+                                    editable
+                                        ? (
+                                            <ItemCounter
+                                                currentValue={product.quantity}
+                                                maxValue={10}
+                                                updatedQuantity={() => { }}
+                                            />
+                                        )
+                                        : (
+                                            <Typography variant='h5'>{product.quantity}{product.quantity > 1 ? 'productos' : 'producto'}</Typography>
+                                        )
                                 }
                             </Box>
                         </Grid>
